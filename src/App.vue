28f52bbf210d0a01c0caa23ffab9399004d132d3
Maksim Chernyh <maxmagic894@gmail.com>
2020-09-12 11:19:16 +1000

    start structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <simon />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import simon from './components/simon'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    simon
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: sans-serif;
 }
 </style>
